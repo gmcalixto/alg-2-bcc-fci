9fa9a1031d7fbc6795a548e2d68f70fb06c7726f
gmcalixto <calixto@Calixto.>
2025-08-28 20:27:03 -0300

    Laços, Incremento e Funções

--- loopwhile.c
+++ loopwhile.c
@@ -4,6 +4,7 @@ int main(){
 
     int cont = 0;
 
+    //teste de continuidade é executado ao início do bloco
     while(cont < 10){
         printf("Valor do contador: %d\n",cont);
         //cont = cont + 1;
